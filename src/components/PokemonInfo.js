1abf69b46604f8ca56caca43fc0f5cc954082c64
Ribich1 <ribich@ukr.net>
2023-07-21 16:12:22 +0200

    before renderig

---
+++ src/components/PokemonInfo.js
@@ -0,0 +1,58 @@
+import { Component } from 'react';
+
+export default class PokemonInfo extends Component {
+  state = {
+    pokemon: null,
+    error: null,
+    status: 'idle',
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevName = prevProps.pokemonName;
+    const nextName = this.props.pokemonName;
+
+    if (prevName !== nextName) {
+      console.log('Изменилось имя покемона');
+      this.setState({ status: 'pending' });
+
+      fetch(`https://pokeapi.co/api/v2/pokemon/${nextName}`)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          return Promise.reject(new Error(`Нет покемона с именем ${nextName}`));
+        })
+        .then(pokemon => this.setState({ pokemon, status: 'resolved' }))
+        .catch(error => this.setState({ error, status: 'rejected' }));
+    }
+  }
+
+  render() {
+    const { pokemon, error, status } = this.state;
+
+    if (status === 'idle') {
+      return <div>Введите имя покемона</div>;
+    }
+
+    if (status === 'pending') {
+      return <div>Загружаем...</div>;
+    }
+
+    if (status === 'rejected') {
+      return <h1>{error.message}</h1>;
+    }
+
+    if (status === 'resolved') {
+      return (
+        <div>
+          <p>{pokemon.name}</p>
+          <img
+            src={pokemon.sprites.other['official-artwork'].front_default}
+            alt={pokemon.name}
+            width="240"
+          />
+        </div>
+      );
+    }
+  }
+}
